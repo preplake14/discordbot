dc9ad68c5d104f43666254225efc6b2edc8715d4
preplake14 <preplake14@gmail.com>
2018-08-14 15:03:04 +0200

    website command

---
+++ commands/simple/website.js
@@ -0,0 +1,27 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class WebSiteCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+             name: 'website',
+             group: 'simple',
+             memberName: 'website',
+             description: 'preplakes website'
+            });
+    }
+    async run(message, args)
+    {
+        var myInfo = new discord.RichEmbed()
+        .setTitle("Website")
+        .setColor(0xFF0000)
+        .setThumbnail(message.author.avatarURL)
+        .setURL("preplak.rf.gd")
+        .setFooter("yeet")
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = WebSiteCommand;
